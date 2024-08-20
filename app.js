3adc655335198d08c3ab3e47c23f6b00985871b4
bankaramol333 <bankaramol333@gmail.com>
2024-08-20 14:48:20 +0530

    visualise function Add slider and playButton event handlers for N-Queen visualization setup

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ const progressBar = document.getElementById("progress-bar");
 const slider = document.getElementById("slider");
 const text = document.getElementById("textt");
 
+let speed,
+  n,
+  tempSpeed,
+  q,
+  Board = 0;
+
+speed = (100 - slider.value) * 10;
+tempSpeedpeed = (100 - slider.value) * 10;
+
 slider.oninput = function () {
   progressBar.style.width = this.value + "%";
+  speed = slider.value;
+  speed = (100 - speed) * 10;
+};
+
+const playButton = document.getElementById("play-button");
+
+playButton.onclick = async function visualise() {
+  const chessBoard = document.getElementById("n-queen-board");
+  const arrangement = document.getElementById("queen-arrangement");
+  const numberbox = document.getElementById("numberbox");
+  n = numberbox.value;
+  q = new Queen();
+
+  if (n > 8) {
+    numberbox.value = "";
+    alert("Too large board size");
+    return;
+  } else if (n < 1) {
+    numberbox.value = "";
+    alert("Too small board size");
+    return;
+  }
 };

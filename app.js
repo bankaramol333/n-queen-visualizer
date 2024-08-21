4407a19aaf46f168428b5240e74d3909449325cf
bankaramol333 <bankaramol333@gmail.com>
2024-08-21 19:28:29 +0530

    Implement core N-Queens solving algorithm with async functions

--- app.js
+++ app.js
@@ -17,8 +17,60 @@ slider.oninput = function () {
   speed = (100 - speed) * 10;
 };
 
-const playButton = document.getElementById("play-button");
+class Queen {
+  constructor() {
+    this.position = Object.assign({}, pos);
+    this.uuid = [];
+  }
+  nQueen = async () => {
+    Board = 0;
+    this.position[`${Board}`] = {};
+    numberbox.disabled = true;
+    await q.solveQueen(Board, 0, n);
+    await q.clearColor(Board);
+    numberbox.disabled = false;
+  };
 
+  solveQueen = async (board, r, n) => {
+    if (r == n) {
+      ++Board;
+      let table = document.getElementById(`table-${this.uuid[Board]}`);
+      for (let k = 0; k < n; ++k) {
+        let row = table.firstChild.childNodes[k];
+        row.getElementsByTagName("td")[this.position[board][k]].innerHTML =
+          queen;
+      }
+      this.position[Board] = this.position[board];
+      return;
+    }
+
+    for (let i = 0; i < n; ++i) {
+      await q.delay();
+      await q.clearColor(board);
+      if (await q.isValid(board, r, i, n)) {
+        await q.delay();
+        await q.clearColor(board);
+        let table = document.getElementById(`table-${this.uuid[board]}`);
+        let row = table.firstChild.childNodes[r];
+        row.getElementsByTagName("td")[i].innerHTML = queen;
+
+        this.position[board][r] = i;
+
+        if (await q.solveQueen(board, r + 1, n)) await q.clearColor(board);
+
+        await q.delay();
+        board = Board;
+        table = document.getElementById(`table-${this.uuid[board]}`);
+        row = table.firstChild.childNodes[r];
+        row.getElementsByTagName("td")[i].innerHTML = "-";
+
+        delete this.position[`${board}`][`${r}`];
+      }
+    }
+  };
+}
+
+const playButton = document.getElementById("play-button");
 playButton.onclick = async function visualise() {
   const chessBoard = document.getElementById("n-queen-board");
   const arrangement = document.getElementById("queen-arrangement");

3fc1c7a79a845def8c056668f2652fac49c0701f
bankaramol333 <bankaramol333@gmail.com>
2024-10-14 14:34:56 +0530

    clear previous contents on board

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const progressBar = document.getElementById("progress-bar");
+"use strict";
+const numberbox = document.getElementById("numberbox");
 const slider = document.getElementById("slider");
-const text = document.getElementById("textt");
+const progressBar = document.getElementById("progress-bar");
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
 
-let speed,
-  n,
+const queen = '<i class="fas fa-chess-queen" style="color:#000"></i>';
+
+let n,
+  speed,
   tempSpeed,
   q,
   Board = 0;
+// Board = 0;
 
+// Creating array for all the possible arrangements of the N-Queen
+let array = [0, 2, 1, 1, 3, 11, 5, 41, 93];
+
+// Used to store the state of the boards;
+let pos = {};
+// let position = {};
+
+// Setting the slider value onSlide
 speed = (100 - slider.value) * 10;
-tempSpeedpeed = (100 - slider.value) * 10;
-
+tempSpeed = speed;
 slider.oninput = function () {
   progressBar.style.width = this.value + "%";
   speed = slider.value;
@@ -20,8 +33,10 @@ slider.oninput = function () {
 class Queen {
   constructor() {
     this.position = Object.assign({}, pos);
+    // this.Board = 0;
     this.uuid = [];
   }
+
   nQueen = async () => {
     Board = 0;
     this.position[`${Board}`] = {};
@@ -32,16 +47,21 @@ class Queen {
   };
 
   isValid = async (board, r, col, n) => {
+    //Setting the current box color to orange
     const table = document.getElementById(`table-${this.uuid[board]}`);
     const currentRow = table.firstChild.childNodes[r];
     const currentColumn = currentRow.getElementsByTagName("td")[col];
     currentColumn.innerHTML = queen;
+    // currentColumn.style.backgroundColor = "#FF9F1C";
     await q.delay();
-    //column check
-    for (let i = r - 1; i >= 0; i--) {
+
+    // Checking the queen in the same column
+    for (let i = r - 1; i >= 0; --i) {
       const row = table.firstChild.childNodes[i];
       const column = row.getElementsByTagName("td")[col];
+
       const value = column.innerHTML;
+
       if (value == queen) {
         column.style.backgroundColor = "#FB5607";
         currentColumn.innerHTML = "-";
@@ -51,7 +71,7 @@ class Queen {
       await q.delay();
     }
 
-    //diagonal check
+    //Checking the upper left diagonal
     for (let i = r - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
       const row = table.firstChild.childNodes[i];
       const column = row.getElementsByTagName("td")[j];
@@ -66,6 +86,7 @@ class Queen {
       await q.delay();
     }
 
+    // Checking the upper right diagonal
     for (let i = r - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
       const row = table.firstChild.childNodes[i];
       const column = row.getElementsByTagName("td")[j];
@@ -84,7 +105,7 @@ class Queen {
   };
 
   clearColor = async (board) => {
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < n; ++j) {
       const table = document.getElementById(`table-${this.uuid[board]}`);
       const row = table.firstChild.childNodes[j];
       for (let k = 0; k < n; ++k)
@@ -115,9 +136,11 @@ class Queen {
 
     for (let i = 0; i < n; ++i) {
       await q.delay();
+      // console.log("outside:" + board);
       await q.clearColor(board);
       if (await q.isValid(board, r, i, n)) {
         await q.delay();
+        // console.log("inside:" + board)
         await q.clearColor(board);
         let table = document.getElementById(`table-${this.uuid[board]}`);
         let row = table.firstChild.childNodes[r];
@@ -129,7 +152,9 @@ class Queen {
 
         await q.delay();
         board = Board;
+        // console.log(this.Board)
         table = document.getElementById(`table-${this.uuid[board]}`);
+        // console.log(JSON.parse(JSON.stringify(table)));
         row = table.firstChild.childNodes[r];
         row.getElementsByTagName("td")[i].innerHTML = "-";
 
@@ -139,28 +164,70 @@ class Queen {
   };
 }
 
-const playButton = document.getElementById("play-button");
 playButton.onclick = async function visualise() {
   const chessBoard = document.getElementById("n-queen-board");
   const arrangement = document.getElementById("queen-arrangement");
-  const numberbox = document.getElementById("numberbox");
+
   n = numberbox.value;
   q = new Queen();
 
   if (n > 8) {
     numberbox.value = "";
-    alert("Too large board size");
+    alert("Queen value is too large");
     return;
   } else if (n < 1) {
     numberbox.value = "";
-    alert("Too small board size");
+    alert("Queen value is too small");
     return;
   }
+
+  // Removing all the of previous execution context
   while (chessBoard.hasChildNodes()) {
     chessBoard.removeChild(chessBoard.firstChild);
   }
   if (arrangement.hasChildNodes()) {
     arrangement.removeChild(arrangement.lastChild);
   }
-  
+
+  const para = document.createElement("p");
+  para.setAttribute("class", "queen-info");
+  para.innerHTML = `For ${n}x${n} board, ${
+    array[n] - 1
+  } arrangements are possible.`;
+  arrangement.appendChild(para);
+
+  //Adding boards to the Div
+  if (chessBoard.childElementCount === 0) {
+    for (let i = 0; i < array[n]; ++i) {
+      q.uuid.push(Math.random());
+      let div = document.createElement("div");
+      let table = document.createElement("table");
+      let header = document.createElement("h4");
+      // div.setAttribute("id", `div-${100 + uuid[i]}`)
+      header.innerHTML = `Board ${i + 1} `;
+      table.setAttribute("id", `table-${q.uuid[i]}`);
+      header.setAttribute("id", `paragraph-${i}`);
+      chessBoard.appendChild(div);
+      div.appendChild(header);
+      div.appendChild(table);
+    }
+  }
+
+  for (let k = 0; k < array[n]; ++k) {
+    let table = document.getElementById(`table-${q.uuid[k]}`);
+    for (let i = 0; i < n; ++i) {
+      const row = table.insertRow(i); // inserting ith row
+      row.setAttribute("id", `Row${i} `);
+      for (let j = 0; j < n; ++j) {
+        const col = row.insertCell(j); // inserting jth column
+        (i + j) & 1
+          ? (col.style.backgroundColor = "#FF9F1C")
+          : (col.style.backgroundColor = "#FCCD90");
+        col.innerHTML = "-";
+        col.style.border = "0.3px solid #373f51";
+      }
+    }
+    await q.clearColor(k);
+  }
+  await q.nQueen();
 };
